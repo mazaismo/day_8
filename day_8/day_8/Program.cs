8e45d173bc221cf19181b6ac78a6b3ff3c1295b0
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-15 19:28:07 +0300

    masiva piemers

--- day_8/day_8/Program.cs
+++ day_8/day_8/Program.cs
@@ -6,8 +6,36 @@ namespace day_8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Paraugs();
+
+
+            Console.WriteLine();
             Console.ReadLine();
+        }
+
+
+
+        static void Paraugs()
+        {
+            int a, b, c, d;
+            a = 1;
+            b = 5;
+            c = 7;
+            d = 6;
+
+            Console.WriteLine(a + " " + b + " " + c + " " + d);
+
+            int[] masivs = new int[4];
+            masivs[0] = 1;
+            masivs[1] = 5;
+            masivs[2] = 7;
+            masivs[3] = 6;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(masivs[i]);
+            }
+
+            //Console.ReadLine();
         }
     }
 }
